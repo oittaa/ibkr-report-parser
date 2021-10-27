20e9649927e6e12b43f1c6a0e566b85c3622239b
oittaa <oittaa@users.noreply.github.com>
2021-10-27 18:30:36 +0200

    Test with GCP storage emulator (#26)

--- test.py
+++ test.py
@@ -1,23 +1,38 @@
 import json
 import os
 import unittest
-
+from gcp_storage_emulator.server import create_server
 from main import app
 from unittest.mock import patch
 
+TEST_BUCKET = "test"
 TEST_URL = "file://" + os.path.abspath(os.getcwd()) + "/test-data/eurofxref-hist.zip"
 
 
 @patch("main.EXCHANGE_RATES_URL", TEST_URL)
 class SmokeTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        os.environ["STORAGE_EMULATOR_HOST"] = "http://localhost:9023"
+        cls._server = create_server(
+            "localhost", 9023, in_memory=True, default_bucket=TEST_BUCKET
+        )
+        cls._server.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._server.stop()
+
     def setUp(self):
         self.app = app.test_client()
         self.assertEqual(app.debug, False)
+        self._server.wipe()
 
     def test_get_main_page(self):
         response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
 
+    @patch("main.BUCKET_ID", TEST_BUCKET)
     def test_get_cron(self):
         response = self.app.get("/cron")
         self.assertEqual(response.status_code, 403)
@@ -56,7 +71,6 @@ class SmokeTests(unittest.TestCase):
         response = self.app.post("/", data=data)
         self.assertEqual(response.status_code, 400)
 
-    @patch("main.LOGGING_LEVEL", "INVALID_LOGGING_LEVEL")
     def test_post_shorting_stocks(self):
         data = {"file": open("test-data/data_shorting_stocks.csv", "rb")}
         response = self.app.post("/?json", data=data)
@@ -98,6 +112,7 @@ class SmokeTests(unittest.TestCase):
         self.assertEqual(data_json["gains"], 5.0)
         self.assertEqual(data_json["losses"], 0.00)
 
+    @patch("main.LOGGING_LEVEL", "INVALID_LOGGING_LEVEL")
     def test_post_invalid_date_html(self):
         data = {"file": open("test-data/data_invalid_date.csv", "rb")}
         response = self.app.post("/", data=data)
@@ -115,6 +130,25 @@ class SmokeTests(unittest.TestCase):
         self.assertEqual(
             data_json["error"], "400 Bad Request: Invalid date '20xx-08-23, 09:33:11'"
         )
+
+    @patch("main._cache", {})
+    @patch("main._MAXCACHE", 0)
+    @patch("main.BUCKET_ID", TEST_BUCKET)
+    def test_caching_filled_from_cron(self):
+        response = self.app.get("/cron", headers={"X-Appengine-Cron": "true"})
+        self.assertEqual(response.status_code, 200)
+        data = {"file": open("test-data/data_single_account.csv", "rb")}
+        response = self.app.post("/?json", data=data)
+        self.assertEqual(response.status_code, 200)
+
+    @patch("main.BUCKET_ID", TEST_BUCKET)
+    def test_caching_from_previous_request(self):
+        data = {"file": open("test-data/data_single_account.csv", "rb")}
+        response = self.app.post("/?json", data=data)
+        self.assertEqual(response.status_code, 200)
+        data = {"file": open("test-data/data_single_account.csv", "rb")}
+        response = self.app.post("/?json", data=data)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
