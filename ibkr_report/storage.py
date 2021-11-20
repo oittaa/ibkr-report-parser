56b667bbcc429bcf5e500f63faf419b81e7e7271
oittaa <oittaa@users.noreply.github.com>
2021-11-20 23:38:51 +0100

    Fix typos, consolidate storage methods (#67)

--- ibkr_report/storage.py
+++ ibkr_report/storage.py
@@ -24,15 +24,23 @@ log = logging.getLogger(__name__)
 class Storage:
     """Storage class to save exchange rates."""
 
-    def save(self, content: CurrencyDict) -> None:
-        """Save CurrencyDict"""
-        log.debug("Save using %s backed.", self.name)
-        del content
+    def save(self, content: CurrencyDict, file_name: str = None) -> None:
+        """Save CurrencyDict to storage."""
+        file_name = file_name or self.get_file_name()
+        log.debug("Save to '%s' using %s backend.", file_name, self.name)
+        self._save(content, file_name)
 
-    def load(self) -> CurrencyDict:
-        """Load CurrencyDict"""
-        log.debug("Load using %s backed.", self.name)
-        return {}
+    def load(self, file_name: str = None) -> CurrencyDict:
+        """Load CurrencyDict from storage."""
+        file_name = file_name or self.get_file_name()
+        log.debug("Load '%s' using %s backend.", file_name, self.name)
+        return self._load(file_name) or {}
+
+    def _save(self, content: CurrencyDict, file_name: str) -> None:
+        pass
+
+    def _load(self, file_name: str) -> CurrencyDict:
+        pass
 
     @property
     def name(self) -> str:
@@ -40,10 +48,14 @@ class Storage:
         return self.__class__.__name__
 
     @staticmethod
-    def get_rates_file_name() -> str:
-        """Generate file name based on the current date."""
-        today = datetime.now().strftime(_DATE)
-        return SAVED_RATES_FILE.format(today)
+    def get_file_name(identifier: str = None) -> str:
+        """Generate a file name based on the current date.
+
+        Optionally you can give your own identifier that distinguishes files.
+        """
+        if identifier is None:
+            identifier = datetime.now().strftime(_DATE)
+        return SAVED_RATES_FILE.format(identifier)
 
     @staticmethod
     def encode(data: CurrencyDict) -> bytes:
@@ -56,27 +68,27 @@ class Storage:
         return json.loads(decompress(data).decode("utf-8"))
 
 
+class StorageDisabled(Storage):
+    """Storage backend that doesn't do anything."""
+
+
 class AmazonS3(Storage):
     """Amazon S3 backend"""
 
-    def __init__(self, bucket_id) -> None:
+    def __init__(self, bucket_id: str = None) -> None:
         boto3 = importlib.import_module("boto3")
 
-        log.debug("Using %s backed.", self.name)
+        log.debug("Using %s backend.", self.name)
         self.bucket_id = bucket_id
         self.aws_s3 = boto3.resource("s3")  # type: ignore
         bucket = self.aws_s3.Bucket(self.bucket_id)
         bucket.create()
 
-    def save(self, content: CurrencyDict) -> None:
-        file_name = self.get_rates_file_name()
-        log.debug("Save using %s backed.", self.name)
+    def _save(self, content: CurrencyDict, file_name: str) -> None:
         obj = self.aws_s3.Object(self.bucket_id, file_name)
         obj.put(Body=self.encode(content))
 
-    def load(self) -> CurrencyDict:
-        file_name = self.get_rates_file_name()
-        log.debug("Load using %s backed.", self.name)
+    def _load(self, file_name: str) -> CurrencyDict:
         obj = self.aws_s3.Object(self.bucket_id, file_name)
         try:
             return self.decode(obj.get()["Body"].read())
@@ -87,11 +99,11 @@ class AmazonS3(Storage):
 class GoogleCloudStorage(Storage):
     """Google Cloud Storage backend"""
 
-    def __init__(self, bucket_id) -> None:
+    def __init__(self, bucket_id: str = None) -> None:
         storage = importlib.import_module("google.cloud.storage")
         exceptions = importlib.import_module("google.cloud.exceptions")
 
-        log.debug("Using %s backed.", self.name)
+        log.debug("Using %s backend.", self.name)
         self.bucket_id = bucket_id
         if os.getenv("STORAGE_EMULATOR_HOST"):
             # Local testing etc.
@@ -104,15 +116,11 @@ class GoogleCloudStorage(Storage):
         except exceptions.NotFound:  # type: ignore
             self.bucket = client.create_bucket(self.bucket_id)
 
-    def save(self, content: CurrencyDict) -> None:
-        file_name = self.get_rates_file_name()
-        log.debug("Save using %s backed.", self.name)
+    def _save(self, content: CurrencyDict, file_name: str) -> None:
         blob = self.bucket.blob(file_name)
         blob.upload_from_string(self.encode(content))
 
-    def load(self) -> CurrencyDict:
-        file_name = self.get_rates_file_name()
-        log.debug("Load using %s backed.", self.name)
+    def _load(self, file_name: str) -> CurrencyDict:
         blob = self.bucket.get_blob(file_name)
         if blob:
             return self.decode(blob.download_as_bytes())
@@ -123,19 +131,15 @@ class LocalStorage(Storage):
     """Local storage backend."""
 
     def __init__(self, storage_dir: str) -> None:
-        log.debug("Using %s backed.", self.name)
+        log.debug("Using %s backend.", self.name)
         self.storage_dir = Path(storage_dir)
         self.storage_dir.mkdir(parents=True, exist_ok=True)
 
-    def save(self, content: CurrencyDict) -> None:
-        log.debug("Save using %s backed.", self.name)
-        file_name = self.get_rates_file_name()
+    def _save(self, content: CurrencyDict, file_name: str) -> None:
         with open(self.storage_dir.joinpath(file_name), "wb") as file:
             file.write(self.encode(content))
 
-    def load(self) -> CurrencyDict:
-        log.debug("Load using %s backed.", self.name)
-        file_name = self.get_rates_file_name()
+    def _load(self, file_name: str) -> CurrencyDict:
         try:
             with open(self.storage_dir.joinpath(file_name), "rb") as file:
                 return self.decode(file.read())
@@ -167,6 +171,6 @@ def get_storage(
     if storage_type is StorageType.LOCAL:
         return LocalStorage(storage_dir)
     if storage_type is StorageType.DISABLED:
-        return Storage()
+        return StorageDisabled()
 
     raise NotImplementedError(f"Not implemented: {storage_type!r}")
