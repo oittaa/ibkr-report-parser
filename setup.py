ee6fee24c48d2e135b5ab2220113a0dc6fd04975
oittaa <8972248+oittaa@users.noreply.github.com>
2024-09-24 15:59:58 +0200

    Drop Python 3.8, add 3.13-dev, Ubuntu 22.04, pylint 3.3.1 (#1062)

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -65,5 +64,5 @@ setup(
         "docker": ["gunicorn==23.0.0"],
         "gcp": ["google-cloud-storage==2.18.2"],
     },
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
