cc9660854be70f41852a783ce195d96936546490
Oittaa <eero@oittaa.net>
2025-03-11 20:22:11 +0700

    Black formatting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" IBKR Report Parser
+"""IBKR Report Parser
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
